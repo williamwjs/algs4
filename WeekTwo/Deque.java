99513ff349c22bdd3685b440bdbdd411e7e619e8
williamwjs <williamwjs.sjtu@gmail.com>
2014-07-22 08:22:29 +0800

    WeekOne Assignment

---
+++ WeekTwo/Deque.java
@@ -0,0 +1,16 @@
+package WeekTwo;
+
+/**
+ * Created by JiashuoWang on 7/22/14.
+ */
+public class Deque<Item> implements Iterable<Item> {
+    public Deque()                           // construct an empty deque
+    public boolean isEmpty()                 // is the deque empty?
+    public int size()                        // return the number of items on the deque
+    public void addFirst(Item item)          // insert the item at the front
+    public void addLast(Item item)           // insert the item at the end
+    public Item removeFirst()                // delete and return the item at the front
+    public Item removeLast()                 // delete and return the item at the end
+    public Iterator<Item> iterator()         // return an iterator over items in order from front to end
+    public static void main(String[] args)   // unit testing
+}
