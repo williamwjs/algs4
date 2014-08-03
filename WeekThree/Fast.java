bd2b779b61ba88c0c0b11a0d2eeff7831ff67d76
williamwjs <williamwjs.sjtu@gmail.com>
2014-08-03 23:09:21 +0800

    WeekThree Assignment - init

---
+++ WeekThree/Fast.java
@@ -0,0 +1,9 @@
+package WeekThree;
+
+/**
+ * Created by JiashuoWang on 8/3/14.
+ */
+public class Fast {
+    public static void main(String[] args) {
+    }
+}
--- WeekThree/Point.java
+++ WeekThree/Point.java
@@ -7,7 +7,17 @@ import java.util.Comparator;
  */public class Point implements Comparable<Point> {
 
     // compare points by slope
-    public final Comparator<Point> SLOPE_ORDER;       // YOUR DEFINITION HERE
+    public final Comparator<Point> SLOPE_ORDER = new Comparator<Point>() {
+        @Override
+        public int compare(Point o1, Point o2) {
+            if (slopeTo(o1) < slopeTo(o2))
+                return -1;
+            else if (slopeTo(o1) > slopeTo(o2))
+                return 1;
+            else
+                return 0;
+        }
+    };
 
     private final int x;                              // x coordinate
     private final int y;                              // y coordinate
@@ -34,12 +44,35 @@ import java.util.Comparator;
     // slope between this point and that point
     public double slopeTo(Point that) {
         /* YOUR CODE HERE */
+        if (this.x == that.x && this.y == that.y)
+            return 0;
+        else if (this.y == that.y)
+            return 0;
+        else if (this.x == that.x && this.y < that.y)
+            return Double.POSITIVE_INFINITY;
+        else if (this.x == that.x && this.y > that.y)
+            return Double.NEGATIVE_INFINITY;
+        else
+            return (double)(this.y - that.y) / (this.x - that.x);
     }
 
     // is this point lexicographically smaller than that one?
     // comparing y-coordinates and breaking ties by x-coordinates
     public int compareTo(Point that) {
         /* YOUR CODE HERE */
+        if (this.y < that.y)
+            return -1;
+        else if (this.y > that.y)
+            return 1;
+        else if (this.y == that.y) {
+            if (this.x < that.y)
+                return -1;
+            else if (this.x > that.x)
+                return 1;
+            else if (this.x == that.x)
+                return 0;
+        }
+        return 0;
     }
 
     // return string representation of this point
