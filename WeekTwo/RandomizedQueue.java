784bdceb73fa9a1a8e9ecfe57e74be3f6b6f06ba
williamwjs <williamwjs.sjtu@gmail.com>
2014-08-03 20:46:19 +0800

    WeekTwo Assignment - v1

--- WeekTwo/RandomizedQueue.java
+++ WeekTwo/RandomizedQueue.java
@@ -1,31 +1,98 @@
 package WeekTwo;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * Created by JiashuoWang on 7/22/14.
  */
 public class RandomizedQueue<Item> implements Iterable<Item> {
+    private static class Node<Item> {
+        Item item;
+        Node<Item> next;
+        Node() {
+            this.item = null;
+            this.next = null;
+        }
+        Node(Item item, Node<Item> next) {
+            this.item = item;
+            this.next = next;
+        }
+    }
+    private Node<Item> head;
+    private Node<Item> tail;
+    private int num;
     // construct an empty randomized queue
     public RandomizedQueue() {
+        head = new Node<Item>();
+        tail = head;
+        num = 0;
     }
     // is the queue empty?
     public boolean isEmpty() {
+        return num == 0;
     }
     // return the number of items on the queue
     public int size() {
+        return num;
     }
     // add the item
     public void enqueue(Item item) {
+        if (item == null) {
+            throw new NullPointerException("Null Pointer!");
+        }
+        tail.next = new Node<Item>(item, null);
+        tail = tail.next;
+        num++;
     }
     // delete and return a random item
     public Item dequeue() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("No item to remove!");
+        }
+        int rand = StdRandom.uniform(num) + 1; // 1-num
+        Node<Item> node = head;
+        for (int i = 0; i < rand-1; i++) { // To the prev of target
+            node = node.next;
+        }
+        Item item = node.next.item;
+        node.next = node.next.next;
+        num--;
+        return item;
     }
     // return (but do not delete) a random item
     public Item sample() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("No item to sample!");
+        }
+        int rand = StdRandom.uniform(num) + 1; // 1-num
+        Node<Item> node = head;
+        for (int i = 0; i < rand; i++) { // To the target
+            node = node.next;
+        }
+        return node.item;
     }
     // return an independent iterator over items in random order
     public Iterator<Item> iterator() {
+        return new Iterator<Item>() {
+            Node<Item> node = head;
+            @Override
+            public boolean hasNext() {
+                return node != tail;
+            }
+
+            @Override
+            public Item next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException("No next() to next!");
+                }
+                node = node.next;
+                return node.item;
+            }
+            public void remove() {
+                throw new UnsupportedOperationException("remove() is not allowed!");
+            }
+        };
     }
     // unit testing
     public static void main(String[] args) {
--- WeekTwo/Subset.java
+++ WeekTwo/Subset.java
@@ -5,6 +5,16 @@ package WeekTwo;
  */
 public class Subset {
     public static void main(String[] args) {
-
+        int k = 3;
+        //int N = 9;
+        RandomizedQueue<String> rq = new RandomizedQueue<String>();
+        //for (int i = 0; i < N; i++) {
+        while (!StdIn.isEmpty()) {
+            rq.enqueue(StdIn.readString());
+        }
+        //for (int i = 0; i < k; i++) {
+        for (int i = 0; i < Integer.parseInt(args[0]); i++) {
+            System.out.println(rq.dequeue());
+        }
     }
 }
